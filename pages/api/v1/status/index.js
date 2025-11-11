f844665797e9a6463b27088102786eab464762f9
Laísa <laisa.fcardoso@gmail.com>
2025-11-11 20:58:27 +0000

    implementações

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,22 @@
 import database from "infra/database.js"
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows)
-  response.status(200).json({ chave: "São acima da média" });
+  const updatedAt = new Date().toISOString();
+  const versionDB = await database.query("SHOW server_version;");
+  const databaseVersionValue = versionDB.rows[0].server_version;
+  const maxConectionDB = await database.query("SHOW max_connections;")
+  const useConectionDB = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname='local_db';")
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: maxConectionDB.rows[0].max_connections,
+        used_connections: useConectionDB.rows[0].count,
+      }
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("GET to /api/v1/status should retun 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toBe(parseUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual("100");
+  expect(responseBody.dependencies.database.used_connections).toEqual(1);
+
 });
